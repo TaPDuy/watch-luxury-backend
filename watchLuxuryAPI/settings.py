9056975dda5b15b1ce77ac76140f792896a2c0d8
TaPDuy <tpduy2001@gmail.com>
2023-03-01 14:42:54 +0700

    Rename user app to authentication

--- watchLuxuryAPI/settings.py
+++ watchLuxuryAPI/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authentication',
+    'django_rename_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
