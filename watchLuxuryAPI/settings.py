01c140a6ca41f43a5167a2018ed9dc74eb6c2c26
TaPDuy <tpduy2001@gmail.com>
2023-03-01 14:44:15 +0700

    Remove django-rename-app

--- watchLuxuryAPI/settings.py
+++ watchLuxuryAPI/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'authentication',
-    'django_rename_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
