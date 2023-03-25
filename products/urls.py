467c33ff20cedaacd22e885117ca997e95c53210
TaPDuy <tpduy2001@gmail.com>
2023-03-25 19:23:27 +0700

    Fix get all products link not working

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import ProductView
+from .views import ProductView, get_products
 
 
 urlpatterns = (
-    path('', ProductView.as_view(), name="products"),
+    path('', get_products, name="products"),
     path('<int:id>/', ProductView.as_view(), name="product"),
 )
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 
 from watchLuxuryAPI.utils import response_code as rescode
 
@@ -11,15 +12,6 @@ from .serializers import ProductSerializer
 
 class ProductView(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly, )
-    
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response({
-            'code': rescode.API_SUCCESS,
-            'msg': f'Retrived {len(products)} product(s)',
-            'data': serializer.data
-        }, status=status.HTTP_200_OK)
 
     def get(self, request, id):
         product = Product.objects.get(pk=id)
@@ -27,5 +19,17 @@ class ProductView(APIView):
         return Response({
             'code': rescode.API_SUCCESS,
             'msg': f'Retrived product',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
+
+@api_view(['GET', ])
+@permission_classes([IsAuthenticatedOrReadOnly, ])
+def get_products(request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response({
+            'code': rescode.API_SUCCESS,
+            'msg': f'Retrived {len(products)} product(s)',
             'data': serializer.data
         }, status=status.HTTP_200_OK)
